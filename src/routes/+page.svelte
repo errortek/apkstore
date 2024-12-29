df3ba5dd4e01a78d2a83e86287236c3556fd9a2c
ErrorTek <jakub.baranski.33@gmail.com>
2024-12-29 19:09:03 +0000

    Implement app version card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,12 +16,31 @@
 </section>
 
 <Card style="width: 100%; margin: 2em auto;">
-  <Content class="mdc-typography--body2">October 24</Content>
+  <Content><p class="mdc-typography--body2">October 24</p></Content>
+</Card>
+<Card style="width: 100%; margin: 2em auto;">
+    <Content>
+        <div class="element">
+            <img src="https://raw.githubusercontent.com/errortek/jpb24/refs/heads/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp" height="32" width="32"/>
+        </div>
+        <div class="element">
+            <h4>jpb24 1.0</h4>
+            <p>by jpb</p>
+        </div>
+        <div class="element">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
 </Card>
 
 <style>
-
 	h1 {
 		width: 100%;
+	}
+	.element {
+	    margin: 10px;
+	    float: left;
 	}
 </style>
