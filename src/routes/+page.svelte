dbdc179df94372c28ff7bf063ead67c63b27931c
ErrorTek <jakub.baranski.33@gmail.com>
2024-12-29 19:26:18 +0000

    Create more cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,104 @@
 </section>
 
 <Card style="width: 100%; margin: 2em auto;">
+  <Content class="mdc-typography--body2">December 29</Content>
+</Card>
+<Card style="width: 100%;">
+    <Content>
+        <div class="element">
+            <img src="https://raw.githubusercontent.com/Surge-OS/surge-dl-components/refs/heads/master/catalog/java/io/material/catalog/ic_launcher-playstore.png" height="64" width="64"/>
+        </div>
+        <div class="element">
+            <h4>SurgeOS Design Language Catalog 1.2</h4>
+            <p>by jpb</p>
+        </div>
+        <div style="float: right;">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
+</Card>
+
+<Card style="width: 100%; margin: 2em auto;">
+  <Content class="mdc-typography--body2">December 28</Content>
+</Card>
+<Card style="width: 100%;">
+    <Content>
+        <div class="element">
+            <img src="https://raw.githubusercontent.com/Surge-OS/surge-dl-components/refs/heads/master/catalog/java/io/material/catalog/ic_launcher-playstore.png" height="64" width="64"/>
+        </div>
+        <div class="element">
+            <h4>SurgeOS Design Language Catalog 1.0</h4>
+            <p>by jpb</p>
+        </div>
+        <div style="float: right;">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
+</Card>
+
+<Card style="width: 100%; margin: 2em auto;">
+  <Content class="mdc-typography--body2">December 26</Content>
+</Card>
+<Card style="width: 100%;">
+    <Content>
+        <div class="element">
+            <img src="https://raw.githubusercontent.com/errortek/jpb24/refs/heads/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp" height="64" width="64"/>
+        </div>
+        <div class="element">
+            <h4>jpb24 1.1</h4>
+            <p>by jpb</p>
+        </div>
+        <div style="float: right;">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
+</Card>
+
+<Card style="width: 100%; margin: 2em auto;">
+  <Content class="mdc-typography--body2">November 20</Content>
+</Card>
+<Card style="width: 100%;">
+    <Content>
+        <div class="element">
+        </div>
+        <div class="element">
+            <h4>Music 1.1</h4>
+            <p>by jpb</p>
+        </div>
+        <div style="float: right;">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
+</Card>
+
+<Card style="width: 100%; margin: 2em auto;">
+  <Content class="mdc-typography--body2">November 7</Content>
+</Card>
+<Card style="width: 100%;">
+    <Content>
+        <div class="element">
+        </div>
+        <div class="element">
+            <h4>Music 1.0</h4>
+            <p>by jpb</p>
+        </div>
+        <div style="float: right;">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
+</Card>
+
+<Card style="width: 100%; margin: 2em auto;">
   <Content class="mdc-typography--body2">October 24</Content>
 </Card>
 <Card style="width: 100%;">
@@ -25,6 +123,46 @@
         </div>
         <div class="element">
             <h4>jpb24 1.0</h4>
+            <p>by jpb</p>
+        </div>
+        <div style="float: right;">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
+</Card>
+
+<Card style="width: 100%; margin: 2em auto;">
+  <Content class="mdc-typography--body2">October 4</Content>
+</Card>
+<Card style="width: 100%;">
+    <Content>
+        <div class="element">
+            <img src="https://raw.githubusercontent.com/errortek/notes/refs/heads/main/app/src/main/ic_launcher-playstore.png" height="64" width="64"/>
+        </div>
+        <div class="element">
+            <h4>Notes 2.0</h4>
+            <p>by jpb</p>
+        </div>
+        <div style="float: right;">
+            <Button>
+                <Label>Download</Label>
+            </Button>
+        </div>
+    </Content>
+</Card>
+
+<Card style="width: 100%; margin: 2em auto;">
+  <Content class="mdc-typography--body2">September 26</Content>
+</Card>
+<Card style="width: 100%;">
+    <Content>
+        <div class="element">
+            <img src="https://raw.githubusercontent.com/errortek/notes/refs/heads/main/app/src/main/ic_launcher-playstore.png" height="64" width="64"/>
+        </div>
+        <div class="element">
+            <h4>Notes 1.5.5</h4>
             <p>by jpb</p>
         </div>
         <div style="float: right;">
