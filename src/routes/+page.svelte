334241b0a89f75a7b6867752243118c2a758047e
ErrorTek <jakub.baranski.33@gmail.com>
2024-12-29 18:00:59 +0000

    Fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,6 @@
 <svelte:head>
 	<title>APKStore</title>
 	<meta name="description" content="APKStore" />
-
-  <!-- Fonts -->
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-  />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
-  />
-
-  <!-- Material Typography -->
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/@material/typography@14.0.0/dist/mdc.typography.css"
-  />
-
-  <!-- SMUI -->
-  <link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
 </svelte:head>
 
 <script lang="ts">
