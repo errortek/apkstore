50c5b7a835ca43ed37baa93f5ba6fc6a55594ce3
ErrorTek <jakub.baranski.33@gmail.com>
2024-12-29 19:13:22 +0000

    UI updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,18 +16,18 @@
 </section>
 
 <Card style="width: 100%; margin: 2em auto;">
-  <Content><p class="mdc-typography--body2">October 24</p></Content>
+  <Content class="mdc-typography--body2">October 24</Content>
 </Card>
 <Card style="width: 100%; margin: 2em auto;">
     <Content>
         <div class="element">
-            <img src="https://raw.githubusercontent.com/errortek/jpb24/refs/heads/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp" height="32" width="32"/>
+            <img src="https://raw.githubusercontent.com/errortek/jpb24/refs/heads/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp" height="64" width="64"/>
         </div>
         <div class="element">
             <h4>jpb24 1.0</h4>
             <p>by jpb</p>
         </div>
-        <div class="element">
+        <div style="float: right;">
             <Button>
                 <Label>Download</Label>
             </Button>
@@ -43,4 +43,10 @@
 	    margin: 10px;
 	    float: left;
 	}
+	img {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      width: 40%;
+    }
 </style>
